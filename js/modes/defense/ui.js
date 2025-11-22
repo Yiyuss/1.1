45cc48db9f10f82d1e3376189369d7a13eb65de7
Yiyuss <155512187+Yiyuss@users.noreply.github.com>
2025-11-22 14:50:10 +0800

    Update ui.js

--- js/modes/defense/ui.js
+++ js/modes/defense/ui.js
@@ -295,7 +295,8 @@
       try {
         const orderedIds = [
           'hp_boost','defense_boost','speed_boost','pickup_range_boost','damage_boost',
-          'damage_specialization','crit_enhance','regen_speed_boost','experience_boost','levelup_action_charges'
+          'damage_specialization','crit_enhance','regen_speed_boost','experience_boost','levelup_action_charges',
+          'defense_gold_boost'
         ];
         const getLv = (id) => (typeof TalentSystem !== 'undefined' && TalentSystem.getTalentLevel) ? (TalentSystem.getTalentLevel(id) || 0) : 0;
         const getDesc = (id) => {
