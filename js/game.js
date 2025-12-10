2d92fdf7a1891639d63d8240d8470a95536ddda3
Yiyuss <155512187+Yiyuss@users.noreply.github.com>
2025-12-10 20:01:20 +0800

    Update game.js

--- js/game.js
+++ js/game.js
@@ -507,7 +507,7 @@ const Game = {
             }
         }
         
-        // 繪製投射物（除連鎖閃電/狂熱雷擊/斬擊與幼妲光輝/幼妲天使聖光/死線戰士，延後至敵人之上）
+        // 繪製投射物（除連鎖閃電/狂熱雷擊/斬擊與幼妲光輝/幼妲天使聖光/死線戰士/死線超人，延後至敵人之上）
         for (const projectile of this.projectiles) {
             if (
                 projectile &&
@@ -517,7 +517,8 @@ const Game = {
                     projectile.weaponType === 'SLASH' ||
                     projectile.weaponType === 'YOUNG_DADA_GLORY' ||
                     projectile.weaponType === 'FRENZY_YOUNG_DADA_GLORY' ||
-                    projectile.weaponType === 'DEATHLINE_WARRIOR'
+                    projectile.weaponType === 'DEATHLINE_WARRIOR' ||
+                    projectile.weaponType === 'DEATHLINE_SUPERMAN'
                 )
             ) {
                 // 延後到前景層（敵人之上）再繪製
@@ -560,7 +561,7 @@ const Game = {
             }
         }
         
-        // 前景層：連鎖閃電/狂熱雷擊/斬擊效果（電弧與火花/GIF）以及幼妲光輝/幼妲天使聖光特效/死線戰士特效
+        // 前景層：連鎖閃電/狂熱雷擊/斬擊效果（電弧與火花/GIF）以及幼妲光輝/幼妲天使聖光特效/死線戰士特效/死線超人特效
         for (const projectile of this.projectiles) {
             if (
                 projectile &&
@@ -570,7 +571,8 @@ const Game = {
                     projectile.weaponType === 'SLASH' ||
                     projectile.weaponType === 'YOUNG_DADA_GLORY' ||
                     projectile.weaponType === 'FRENZY_YOUNG_DADA_GLORY' ||
-                    projectile.weaponType === 'DEATHLINE_WARRIOR'
+                    projectile.weaponType === 'DEATHLINE_WARRIOR' ||
+                    projectile.weaponType === 'DEATHLINE_SUPERMAN'
                 )
             ) {
                 projectile.draw(this.ctx);
@@ -762,6 +764,12 @@ const Game = {
                 const diffId = this.selectedDifficultyId || 'EASY';
                 if (diffId === 'HARD') {
                     Achievements.unlock('HARD_CLEAR');
+                }
+                // 洛可洛斯特通關廁所：解鎖死線超人成就
+                const charId = (this.selectedCharacter && this.selectedCharacter.id) ? this.selectedCharacter.id : null;
+                const mapId = (this.selectedMap && this.selectedMap.id) ? this.selectedMap.id : null;
+                if (charId === 'rokurost' && mapId === 'city') {
+                    Achievements.unlock('ROKUROST_CITY_CLEAR');
                 }
             }
         } catch(_) {}
