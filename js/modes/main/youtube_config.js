0486e8cc09aa0a2873e6d3508f683d419b76d393
Yiyuss <155512187+Yiyuss@users.noreply.github.com>
2025-12-13 15:27:33 +0800

    Update youtube_config.js

--- js/modes/main/youtube_config.js
+++ js/modes/main/youtube_config.js
@@ -3,7 +3,7 @@
 // 獲取方式請參考：README_YOUTUBE_API.md
 
 window.YouTubeAPIConfig = {
-  apiKey: 'AIzaSyCncIz1QktmWKoWvVWs-hGrGR90B1BSSJw',
-  clientId: '780755313842-p0ncu8s7p0cj07h5urijrogchbjvutjb.apps.googleusercontent.com'
+  apiKey: 'AIzaSyAN2BD04Utez2upIHhuxFEBO36-Fj1U6jM',
+  clientId: '867518315398-93dj2d360arl32v8s05ujalr4fb3mrbc.apps.googleusercontent.com'
 };
 
